54b74f806652aef4e8b98527f54a087bb6b7a20f
s.pavlov@mininform74.ru <s.pavlov@mininform74.ru>
2023-10-24 12:06:43 +0500

    Стилизовал форму логина

--- app/(full-page)/auth/login/page.tsx
+++ app/(full-page)/auth/login/page.tsx
@@ -25,42 +25,43 @@ const LoginPage = () => {
         shift.current?.classList.remove('itr-right-panel-active');
     }
 
+    const innerCss = `
+        .itr-right-panel-active {
+            background-color: orange;
+        }`
+
     return (
         <React.Fragment>
             <div className="surface-ground flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden">
                 <div ref={shift} className="itr-login-form">
                     <div className="itr-form-container itr-sign-up-container">
                         <form action="#">
-                            <h1 className="itr-h1">Восстановление пароля</h1>
+                            <h1>Восстановление пароля</h1>
                             <input type="email" placeholder="Адрес электронной почты" value={rest} onChange={e => setRest(e.target.value)}/>
-                            <button className="itr-button" disabled={!rest} >Отправить</button>
+                            <button disabled={!rest} >Отправить</button>
                         </form>
                     </div>
                     <div className="itr-form-container itr-sign-in-container">
                         <form action="#">
-                            <h1 className="itr-h1">Вход в систему</h1>
+                            <h1>Вход в систему</h1>
                             <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)}/>
                             <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}/>
-                            <button className="itr-button" disabled={!email || !password} >Войти</button>
+                            <button disabled={!email || !password} >Войти</button>
                         </form>
                     </div>
                     <div className="itr-overlay-container">
                         <div className="itr-overlay">
                             <div className="itr-overlay-panel itr-overlay-left">
-                                <img className="itr-overlay-logo" src={'/layout/images/logo.svg'} alt =""/>
-                                <h1 className="itr-h1">{$app_variables.TITLE}</h1>
-                                <p className="itr-p">На указанный адрес электронной почты будет выслано письмо со ссылкой на форму восстановления пароля</p>
-                                <button className="itr-ghost itr-button" id="signIn"
-                                        onClick={() => leftToRight()}
-                                >Войти в систему</button>
+                                <img src={'/layout/images/logo.svg'} alt =""/>
+                                <h1>{$app_variables.TITLE}</h1>
+                                <p>На указанный адрес электронной почты будет выслано письмо со ссылкой на форму восстановления пароля</p>
+                                <button className="itr-ghost" id="signIn" onClick={() => leftToRight()}>Войти в систему</button>
                             </div>
                             <div className="itr-overlay-panel itr-overlay-right">
-                                <img className="itr-overlay-logo" src={'/layout/images/logo.svg'} alt =""/>
-                                <h1 className="itr-h1">{$app_variables.TITLE}</h1>
-                                <p className="itr-p">Для входа в систему введите адрес электронной почты и пароль</p>
-                                <button className="itr-ghost itr-button" id="signUp"
-                                        onClick={() => rightToLeft()}
-                                >Восстановить пароль</button>
+                                <img src={'/layout/images/logo.svg'} alt =""/>
+                                <h1>{$app_variables.TITLE}</h1>
+                                <p>Для входа в систему введите адрес электронной почты и пароль</p>
+                                <button className="itr-ghost" id="signUp" onClick={() => rightToLeft()}>Восстановить пароль</button>
                             </div>
                         </div>
                     </div>
