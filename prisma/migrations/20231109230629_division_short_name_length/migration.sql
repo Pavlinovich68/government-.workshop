a2080407ddabcb7715d61c80f150bbcf96f8f0bd
Pavlinovich68 <54423661+Pavlinovich68@users.noreply.github.com>
2023-11-10 04:09:53 +0500

    seed divisions

---
+++ prisma/migrations/20231109230629_division_short_name_length/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "division" ALTER COLUMN "short_name" SET DATA TYPE VARCHAR(50);
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,68 +1,126 @@
+import { Extensions } from "@prisma/client/runtime/library";
 import prisma from "./client";
 import crypto from "./crypt";
 import {appRoles} from "./roles";
 import bcrypt from "bcrypt";
 
-
-const divisions = [
-   {
-      name: "Губернатор Челябинской области",
-      short_name: "Губернатор",
-      contacts: null,
-      childrens: [
-         {
-            name: "Аппарат Губернатора и Правительства Челябинской области",
-            short_name: "Аппарат Правительства",
-            contacts: null,
-            childrens: [
-               {
-                  name: "Первый заместитель Губернатора Челябинской области",
-                  short_name: "Приемная Мамина В.В.",
-                  contacts: null,
-                  childrens: []
-               },
-               {
-                  name: "Первый заместитель Губернатора Челябинской области",
-                  short_name: "Приемная Гехт И.А.",
-                  contacts: null,
-                  childrens: []
-               },
-            ]
-         }
-      ]
-   }
-];
+// TODO Seed
+const divisions = {
+   name: "Губернатор Челябинской области",
+   short_name: "Губернатор",
+   contacts: null,
+   childrens: [
+      {
+         name: "Аппарат Губернатора и Правительства Челябинской области",
+         short_name: "Аппарат Правительства",
+         contacts: null,
+         childrens: [
+            {
+               name: "Первый заместитель Губернатора Челябинской области",
+               short_name: "Приемная Мамина В.В.",
+               contacts: null,
+               childrens: [
+                  {
+                     name: "Заместитель руководителя Аппарата Губернатора и Правительства",
+                     short_name: "Приемная Козлова А.С.",
+                     contacts: "",
+                     childrens: [
+                        {
+                           name: "Министерство информационных технологий, связи и цифрового развития",
+                           short_name: "Минцифры",
+                           contacts: "",
+                           childrens: [
+                              {
+                                 name: "ОГБУ \"Челябинский региональный центр навигационно-информационных технологий\"",
+                                 short_name: "ОГБУ ЧРЦНИТ",
+                                 contacts: "",
+                                 childrens: [
+                                    {
+                                       name: "Служба администрирования",
+                                       short_name: "Админы",
+                                       contacts: "+7 (351) 211-68-27",
+                                       childrens: []
+                                    }
+                                 ]
+                              },
+                              {
+                                 name: "ОГКУ \"Центр информационно-технического обслуживания\"",
+                                 short_name: "ОГКУ ЦИТО",
+                                 contacts: "",
+                                 childrens: []
+                              }
+                           ]
+                        }
+                     ]
+                  }
+               ]
+            },
+            {
+               name: "Первый заместитель Губернатора Челябинской области",
+               short_name: "Приемная Гехт И.А.",
+               contacts: null,
+               childrens: []
+            },
+         ]
+      }
+   ]
+};
 
 async function main() {
-   const div = await prisma.division.upsert({
-      where: {name: 'Служба администрирования'},
-      update: {
-         contacts: "+7 (351) 211-68-27"
-      },
-      create: {
-            name: 'Служба администрирования',
-            short_name: "Админы"
+   const upsertDivision = async (model: any, parentId: number | null) => {
+      console.log(`Upsert division:`, model, '\n Parent division:', parentId);
+      const result = await prisma.division.upsert({
+         where: {name: model.name},
+         update: {
+            short_name: model.short_name,
+            contacts: model.contacts,
+            parent_id: parentId
+         },
+         create: {
+               name: model.name,
+               short_name: model.short_name,
+               contacts: model.contacts,
+               parent_id: parentId
+         }
+      });
+      if (model.childrens && model.childrens.length > 0){
+         for (const item of model.childrens){
+            await upsertDivision(item, result.id);
+         }
+      }
+      return result;
+   }
+
+   upsertDivision(divisions, null);
+
+
+   const division = await prisma.division.findUnique({
+      where: {
+         name: 'Служба администрирования'
       }
    });
 
+   if (division) {
+      const hashPassword = await bcrypt.hashSync("Administrator1!", 8);
 
-   const hashPassword = await bcrypt.hashSync("Administrator1!", 8);
-
-   const row = await prisma.users.upsert({
-      where: {email: 'administrator@localhost'},
-      update: {
-         name: 'Администратор системы',
-         roles: appRoles
-      },
-      create: {
-            email: 'administrator@localhost',
+      const row = await prisma.users.upsert({
+         where: {email: 'administrator@localhost'},
+         update: {
             name: 'Администратор системы',
-            begin_date: new Date(),
-            password: hashPassword,
-            division_id: div.id,
             roles: appRoles
-      }
-   })
+         },
+         create: {
+               email: 'administrator@localhost',
+               name: 'Администратор системы',
+               begin_date: new Date(),
+               password: hashPassword,
+               division_id: division.id,
+               roles: appRoles
+         }
+      })
+   } else {
+      throw new Error("Не удалось найти подразделение администраторов!");
+   }
 }
 
 main().then(async () => {
