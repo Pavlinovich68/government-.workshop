7d2fb46f5dcccea02114a1bb88270dfb17ff1fd2
s.pavlov@mininform74.ru <s.pavlov@mininform74.ru>
2023-11-10 20:53:50 +0500

    read division list

--- app/(main)/workplace/references/divisions/page.jsx
+++ app/(main)/workplace/references/divisions/page.jsx
@@ -1,12 +1,54 @@
 'use client'
 import React, {useEffect, useRef, useState} from "react";
+import { TreeTable } from 'primereact/treetable';
+import { Column } from 'primereact/column';
+import { Button } from 'primereact/button'
 
 const Divisions = () => {
+   const [divisions, setDivisions] = useState([]);
+
+   useEffect(() => {
+      // Получение данных по подразделениям
+
+   }, []);
+
+   const togglerTemplate = (node, options) => {
+      if (!node) {
+         return;
+      }
+
+      const expanded = options.expanded;
+      const iconClassName = classNames('p-treetable-toggler-icon pi pi-fw', {
+         'pi-caret-right': !expanded,
+         'pi-caret-down': expanded
+      });
+
+      return (
+         <button type="button" className="p-treetable-toggler p-link" style={options.buttonStyle} tabIndex={-1} onClick={options.onClick}>
+            <span className={iconClassName} aria-hidden="true"></span>
+         </button>
+      );
+   };
+
+   const actionTemplate = () => {
+      return (
+         <div className="flex flex-wrap gap-2">
+            <Button type="button" icon="pi pi-search" rounded></Button>
+            <Button type="button" icon="pi pi-pencil" severity="success" rounded></Button>
+         </div>
+      );
+   };
+
    return (
       <div className="grid">
          <div className="col-12">
             <div className="card">
                <h3>Подразделения</h3>
+               <TreeTable value={divisions} togglerTemplate={togglerTemplate} tableStyle={{ minWidth: '50rem' }} rowHover="true" stripedRows="true">
+                  <Column field="name" header="Наименование структурного подразделения" expander style={{width: '70%'}}></Column>
+                  <Column field="contacts" header="Контактная информация"></Column>
+                  <Column body={actionTemplate} headerClassName="w-10rem" />
+               </TreeTable>
             </div>
          </div>
       </div>
