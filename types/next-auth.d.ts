4f89ea0c1205661832927ff045c323f3f87caca2
Pavlinovich68 <54423661+Pavlinovich68@users.noreply.github.com>
2023-12-03 20:16:14 +0500

    !

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,9 +1,9 @@
 import NextAuth, {DefaultSession} from "next-auth";
 
 declare module "next-auth" {
-    interface Session {
-        user: {
-            password: string
-        } & DefaultSession["user"]
-    }
+   interface Session {
+      user: {
+         password: string
+      } & DefaultSession["user"]
+   }
 }
