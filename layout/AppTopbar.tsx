708f009422f4b5813c18568daed9c8c4f27911ec
s.pavlov@mininform74.ru <s.pavlov@mininform74.ru>
2023-12-20 07:15:33 +0500

    Диалог профиля

--- layout/AppTopbar.tsx
+++ layout/AppTopbar.tsx
@@ -7,9 +7,10 @@ import { AppTopbarRef } from '../types/types';
 import { LayoutContext } from './context/layoutcontext';
 import Image from "next/image";
 import {signOut} from "next-auth/react";
-import { SplitButton } from 'primereact/splitbutton';
 import {useSession} from "next-auth/react";
 import { Avatar } from 'primereact/avatar';
+import {ICardRef} from '@/models/ICardRef';
+import ItrCard from "@/components/ItrCard";
 
 
 const getAvatar = async (id: number) => {
@@ -32,6 +33,7 @@ const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
    const topbarmenuRef = useRef(null);
    const topbarmenubuttonRef = useRef(null);
    const [avatar, setAvatar] = useState('');
+   const profileEditor = useRef<ICardRef>(null);
 
    console.log(session?.user);
    //@ts-ignore
@@ -48,41 +50,61 @@ const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
       topbarmenubutton: topbarmenubuttonRef.current
    }));
 
+   const profileCard = () => {
+      return (
+         <form onSubmit={saveProfile}>
+            <div className="card p-fluid">
+            </div>
+         </form>
+      );
+   }
+
+   const saveProfile = async () => {
+
+   }
+
    return (
       <div className="layout-topbar">
-            <button  type="button" className="p-link layout-menu-button layout-topbar-button" onClick={onMenuToggle} title='Menubar'>
-               <i className="pi pi-bars" />
+         <button  type="button" className="p-link layout-menu-button layout-topbar-button" onClick={onMenuToggle} title='Menubar'>
+            <i className="pi pi-bars" />
+         </button>
+
+         <Link href="/" className="layout-topbar-logo">
+            <Image src={`/layout/images/logo.svg`}
+                  alt="Logo"
+                  width="0"
+                  height="0"
+                  sizes="100vw"
+                  style={{ width: '32px', height: 'auto' }}
+            />
+            <span>Центр коммуникаций Правительства Челябинской области</span>
+         </Link>
+
+         <button ref={topbarmenubuttonRef} type="button" className="p-link layout-topbar-menu-button layout-topbar-button" onClick={showProfileSidebar} title='Dropdown'>
+            <i className="pi pi-ellipsis-v" />
+         </button>
+
+         <div ref={topbarmenuRef} className={classNames('layout-topbar-menu', { 'layout-topbar-menu-mobile-active': layoutState.profileSidebarVisible })}>
+            <button type="button" className="p-link layout-topbar-button">
+               <i className="pi pi-bell"></i>
+               <span>События</span>
             </button>
+            <a></a>
+            {avatar ? <Avatar image={avatar} size="large" shape="circle" className='itr-avatar' onClick={()=>{if (profileEditor.current) { profileEditor.current.visible(true); }}}/> :
+            <Avatar icon="pi pi-user" size="large" shape="circle" className='itr-avatar'/>}
 
-            <Link href="/" className="layout-topbar-logo">
-               <Image src={`/layout/images/logo.svg`}
-                     alt="Logo"
-                     width="0"
-                     height="0"
-                     sizes="100vw"
-                     style={{ width: '32px', height: 'auto' }}
-               />
-               <span>Центр коммуникаций Правительства Челябинской области</span>
-            </Link>
-
-            <button ref={topbarmenubuttonRef} type="button" className="p-link layout-topbar-menu-button layout-topbar-button" onClick={showProfileSidebar} title='Dropdown'>
-               <i className="pi pi-ellipsis-v" />
+            <button type="button" className="p-link layout-topbar-button" onClick={() => {signOut({callbackUrl: "/login"})}}>
+               <i className="pi pi-sign-out"></i>
+               <span>Выход</span>
             </button>
-
-            <div ref={topbarmenuRef} className={classNames('layout-topbar-menu', { 'layout-topbar-menu-mobile-active': layoutState.profileSidebarVisible })}>
-               <button type="button" className="p-link layout-topbar-button">
-                  <i className="pi pi-calendar"></i>
-                  <span>Calendar</span>
-               </button>
-               <a></a>
-               {avatar ? <Avatar image={avatar} size="large" shape="circle" className='itr-avatar'/> :
-               <Avatar icon="pi pi-user" size="large" shape="circle" className='itr-avatar'/>}
-
-               <button type="button" className="p-link layout-topbar-button" onClick={() => {signOut({callbackUrl: "/login"})}}>
-                  <i className="pi pi-sign-out"></i>
-                  <span>Settings</span>
-               </button>
-            </div>
+         </div>
+         <ItrCard
+            header="Профиль пользователя"
+            dialogStyle={{ width: '20vw' }}
+            save={saveProfile}
+            body={profileCard()}
+            ref={profileEditor}
+         />
       </div>
    );
 });
